34baab57874dbd920402eab39041f88039d86105
Gzman <ganzmann@protonmail.com>
2021-09-02 01:44:06 +0200

    Added Logo for Header

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { SiTodoist } from "react-icons/si"
 import "./Header.css"
 
 const Logo = () => {
     return (
         <div className="header-logo">
+            <SiTodoist className="logo-icon" />
             <h1 className="logo">Todo App</h1>
         </div>
     )
